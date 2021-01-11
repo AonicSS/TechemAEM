9f8924588a928cab7551fe6623ad28437aec5779
AndreeaStoiu <andreea.gina.stoiu@ibm.com>
2021-01-11 15:03:39 +0200

    general-container and refactored image-tile

---
+++ core/src/main/java/com/techem/core/models/GeneralContainer.java
@@ -0,0 +1,33 @@
+package com.techem.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+import javax.inject.Inject;
+
+
+@Model(adaptables = Resource.class, adapters = GeneralContainer.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        resourceType = "techem/components/general-container")
+public class GeneralContainer {
+
+    /**
+     * The current resource.
+     */
+    @Inject
+    private Resource resource;
+
+    @Inject
+    @Default(values = "Headline")
+    private String headline;
+
+    public String getHeadline() {
+        if (this.headline == null) {
+            this.headline = this.resource.getValueMap().get("headline", String.class);
+        }
+        return headline;
+    }
+
+}
---
+++ core/src/main/java/com/techem/core/models/ImageTile.java
@@ -0,0 +1,73 @@
+package com.techem.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+import javax.inject.Inject;
+
+@Model(adaptables = Resource.class, adapters = ImageTile.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        resourceType = "techem/components/image-tile")
+public class ImageTile {
+
+    /**
+     * The current resource.
+     */
+    @Inject
+    private Resource resource;
+
+    @Inject
+    @Default(values = "Headline")
+    private String headline;
+
+    @Inject
+    @Default(values = "Body")
+    private String body;
+
+    @Inject
+    @Default(values = "white")
+    private String background;
+
+    @Inject
+    private String linkURL2;
+
+    @Inject
+    private String fileReference2;
+
+    public String getHeadline() {
+        if (this.headline == null) {
+            this.headline = this.resource.getValueMap().get("headline", String.class);
+        }
+        return headline;
+    }
+
+    public String getBody() {
+        if (this.body == null) {
+            this.body = this.resource.getValueMap().get("body", String.class);
+        }
+        return body;
+    }
+
+    public String getBackground() {
+        if (this.background == null) {
+            this.background = this.resource.getValueMap().get("background", String.class);
+        }
+        return background;
+    }
+
+    public String getLinkURL2() {
+        if (this.linkURL2 == null) {
+            this.linkURL2 = this.resource.getValueMap().get("linkURL2", String.class);
+        }
+        return linkURL2;
+    }
+
+    public String getFileReference2() {
+        if (this.fileReference2 == null) {
+            this.fileReference2 = this.resource.getValueMap().get("fileReference2", String.class);
+        }
+        return fileReference2;
+    }
+}
