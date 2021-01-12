9cf5167c1e7dabd0f51afcd0dd9e2b87126464b3
AndreeaStoiu <andreea.gina.stoiu@ibm.com>
2021-01-12 15:21:29 +0200

    added tabs-icon-number and text limit

---
+++ core/src/main/java/com/techem/core/models/TabsIconNumber.java
@@ -0,0 +1,42 @@
+package com.techem.core.models;
+
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+
+import javax.inject.Inject;
+
+@Model(adaptables = Resource.class, adapters = TabsIconNumber.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        resourceType = "techem/components/tabs-icon-number")
+public class TabsIconNumber {
+
+    /**
+     * The current resource.
+     */
+    @Inject
+    private Resource resource;
+
+    @Inject
+    @Default(values = "Headline for the module")
+    private String headline;
+
+    @Inject
+    private String subtitle;
+
+    public String getHeadline() {
+        if (this.headline == null) {
+            this.headline = this.resource.getValueMap().get("headline", String.class);
+        }
+        return headline;
+    }
+
+    public String getSubtitle() {
+        if (this.subtitle == null) {
+            this.subtitle = this.resource.getValueMap().get("subtitle", String.class);
+        }
+        return subtitle;
+    }
+}
