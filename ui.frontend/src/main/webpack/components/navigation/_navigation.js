cfa11349c6ba02906cfb47f601b9b52493502a44
AzureAD\MihaiConstantin <mihai.constantin2@ibm.com>
2021-01-18 09:52:39 +0200

    Navigation sccs and js update

--- ui.frontend/src/main/webpack/components/navigation/_navigation.js
+++ ui.frontend/src/main/webpack/components/navigation/_navigation.js
@@ -1,111 +1,159 @@
-(function() {
+(function () {
     "use strict";
 
     function NotificationModule(el) {
-            el.dataset.initialized = true;
+        el.dataset.initialized = true;
 
-            this.$el = $(el);
+        this.$el = $(el);
 
-            this.selectors = {
-                  headerDropdown: '.header__dropdown-content',
-                  navigationLink: '.navigation__link',
-                  closeButton : '.close',
-                  headerButton: ".header__button",
-                  active: "navigation-active",
-                  hidden: 'header__dropdown-hidden'
-            };
+        this.selectors = {
+            headerDropdown: '.header__dropdown-content',
+            navigationLink: '.navigation__link',
+            closeButton: '.close',
+            headerButton: ".header__button",
+            active: "navigation-active",
+            hidden: 'header__dropdown-hidden'
+        };
 
-            this.atributes = {
-                dropdownItem: "dropdown-item",
-                link: "button-link",
-                content: "content",
-                protocol: "https://"
-            };
+        this.atributes = {
+            dropdownItem: "dropdown-item",
+            link: "button-link",
+            content: "content",
+            protocol: "https://"
+        };
 
-            this.init = function() {
-                this.showAndHideDropdown();
-                this.closeDropDown();
-                this.redirectButton();
-            }.bind(this);
+        this.init = function () {
+            this.showAndHideDropdown();
+            this.closeDropDown();
+            this.redirectButton();
+        }.bind(this);
 
-            this.showAndHideDropdown = function() {
-               var $links = this.$el.find(this.selectors.navigationLink);
+        this.showAndHideDropdown = function () {
+            var $links = this.$el.find(this.selectors.navigationLink);
 
-               $links.on('click', (event) => {
-                    var $target = $(event.target);
-                    var $link = undefined;
+            $links.on('click', (event) => {
+                var $target = $(event.target);
+                var $link = undefined;
 
-                    if($target.is("li")) {
-                        $link = $target;
-                    } else {
-                        $link = $target.parent();
-                    }
-
-                    var navItemAttr = $link && $link.attr(this.atributes.dropdownItem);
-
-                    if(navItemAttr.trim().length > 0) {
-                          var $dropdownItem =  this.$el.find("div[name='" + navItemAttr + "']");
-
-                          if($dropdownItem && $dropdownItem.length > 0) {
-                              this.hideAllDropDowns();
-                              this.removeActive();
-                              $dropdownItem.removeClass(this.selectors.hidden);
-                              $link.addClass(this.selectors.active);
-                          }
-                    }
-               });
-            }.bind(this);
-
-            this.closeDropDown = function() {
-                var $closeButton = this.$el.find(this.selectors.closeButton);
-
-                $closeButton.on('click', () => {
-                    this.hideAllDropDowns();
-                });
-            }.bind(this);
-
-            this.hideAllDropDowns = function() {
-                var $dropdownItems = this.$el.find(this.selectors.headerDropdown);
-                if($dropdownItems && $dropdownItems.length > 0) {
-                    $dropdownItems.addClass(this.selectors.hidden);
+                if ($target.is("li")) {
+                    $link = $target;
+                } else {
+                    $link = $target.parent();
                 }
-            }.bind(this);
 
-            this.removeActive = function () {
-             var $activeItems = this.$el.find(this.selectors.navigationLink);
-             if($activeItems && $activeItems.length > 0) {
-                 $activeItems.removeClass(this.selectors.active);
-             }
-            }.bind(this);
+                var navItemAttr = $link && $link.attr(this.atributes.dropdownItem);
 
-            this.redirectButton = function() {
-                 var $headerButton = this.$el.find(this.selectors.headerButton);
+                if (navItemAttr.trim().length > 0) {
+                    var $dropdownItem = this.$el.find("div[name='" + navItemAttr + "']");
 
-                 $headerButton.on('click', () => {
-                     var redirectLink = $headerButton && $headerButton.attr(this.atributes.link);
+                    if ($dropdownItem && $dropdownItem.length > 0) {
+                        this.hideAllDropDowns();
+                        this.removeActive();
+                        $dropdownItem.removeClass(this.selectors.hidden);
+                        $link.addClass(this.selectors.active);
+                    }
+                }
+            });
+        }.bind(this);
 
-                     if(redirectLink.includes(this.atributes.content)) {
-                        window.location.href = window.location.origin  + redirectLink;
-                     } else {
-                         window.location.href =  redirectLink.includes(this.atributes.protocol)  ?
-                                                 redirectLink : this.atributes.protocol + redirectLink;
-                     }
-                 });
+        this.closeDropDown = function () {
+            var $closeButton = this.$el.find(this.selectors.closeButton);
 
-            }.bind(this);
+            $closeButton.on('click', () => {
+                this.hideAllDropDowns();
+            });
+        }.bind(this);
 
-            this.init();
+        this.hideAllDropDowns = function () {
+            var $dropdownItems = this.$el.find(this.selectors.headerDropdown);
+            if ($dropdownItems && $dropdownItems.length > 0) {
+                $dropdownItems.addClass(this.selectors.hidden);
+            }
+        }.bind(this);
 
-            return this;
+        this.removeActive = function () {
+            var $activeItems = this.$el.find(this.selectors.navigationLink);
+            if ($activeItems && $activeItems.length > 0) {
+                $activeItems.removeClass(this.selectors.active);
+            }
+        }.bind(this);
+
+        this.redirectButton = function () {
+            var $headerButton = this.$el.find(this.selectors.headerButton);
+
+            $headerButton.on('click', () => {
+                var redirectLink = $headerButton && $headerButton.attr(this.atributes.link);
+
+                if (redirectLink.includes(this.atributes.content)) {
+                    window.location.href = window.location.origin + redirectLink;
+                } else {
+                    window.location.href = redirectLink.includes(this.atributes.protocol) ?
+                        redirectLink : this.atributes.protocol + redirectLink;
+                }
+            });
+
+        }.bind(this);
+
+        this.init();
+
+        return this;
     }
 
-    $(document).ready(function() {
+    $(document).ready(function () {
         var $notification = $("header[data-component-name='navigation']");
-        Array.prototype.forEach.call($notification, function(element) {
+        Array.prototype.forEach.call($notification, function (element) {
             if (element && element.dataset.initialized !== "true") {
                 new NotificationModule(element);
             }
         });
     });
 }());
+$('.header__search-icon').mouseover(function () {
+    $(".header__search-icon").css("display", "none");
+    $(".cmp-search").show("slow");
+});
+$('.cmp-search').mouseout(function () {
+    if (!$('.cmp-search__input').val() || $('.cmp-search__input').val() === "Search") {
+        $(".cmp-search").hide();
+        $(".header__search-icon").css("display", "block");
+    }
+});
+/* ---------------------------------------------
+burger menu
+ --------------------------------------------- */
+ function burgermenuinit() {
+    var clickDelay = 500,
+        clickDelayTimer = null;
 
+    $('.burger-click-region').on('click', function() {
+        if (clickDelayTimer === null) {
+            var $burger = $(this);
+            $burger.toggleClass('active');
+            $burger.parent().toggleClass('is-open');
+            $('nav.navigation').slideDown();
+
+            if (!$burger.hasClass('active')) {
+                $burger.addClass('closing');
+                $('nav.navigation').slideUp();
+            }
+            clickDelayTimer = setTimeout(function() {
+                $burger.removeClass('closing');
+                clearTimeout(clickDelayTimer);
+                clickDelayTimer = null;
+            }, clickDelay);
+        }
+    });
+}
+$(document).ready(function() {
+    burgermenuinit();
+});
+
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 500) {
+        $(".header").addClass("sticky");
+    } else {
+        $(".header").removeClass("sticky");
+    }
+});
