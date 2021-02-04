6e3ab008fb485848f994a2c0a4b7ccb34d86c703
Ovidiu Schintee <ovidiu.schintee@ibm.com>
2021-02-04 18:50:41 +0200

    Add Image Gallery Carousel component

---
+++ core/src/main/java/com/techem/core/models/CarouselImageGallery.java
@@ -0,0 +1,27 @@
+package com.techem.core.models;
+
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
+
+import javax.inject.Inject;
+
+@Model(adaptables = Resource.class, adapters = CarouselImageGallery.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        resourceType = "techem/components/carousel-image-gallery")
+public class CarouselImageGallery {
+
+    /**
+     * The current resource.
+     */
+    @Inject
+    private Resource resource;
+
+    @ValueMapValue(name = "loop")
+    private Boolean loop;
+
+    public Boolean getLoop() {
+        return loop;
+    }
+}
