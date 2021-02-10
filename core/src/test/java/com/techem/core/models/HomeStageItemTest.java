1c680ecc36a3206bf824efaef33753886640ac32
AndreeaStoiu <andreea.gina.stoiu@ibm.com>
2021-02-10 14:14:39 +0200

    unit tests for stage Homepage models

---
+++ core/src/test/java/com/techem/core/models/HomeStageItemTest.java
@@ -0,0 +1,60 @@
+package com.techem.core.models;
+
+import com.day.cq.wcm.api.Page;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.api.resource.Resource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(AemContextExtension.class)
+class HomeStageItemTest
+{
+
+	private HomeStageItem homeStageItem;
+
+	private static final String CONTENT_TEST_RESOURCE = "/content/mypage";
+	private static final String RESOURCE_TYPE = "sling:resourceType";
+	private static final String COMPONENT_RESOURCE_TYPE = "techem/components/service-item";
+
+	private static final String HEADLINE = "headline";
+	private static final String LINK_URL_2 = "linkURL2";
+
+	private static final String EXPECTED_HEADLINE = "Hello from home stage item";
+	private static final String EXPECTED_LINK_URL_2 = "www.techem.de";
+
+	private Page page;
+	private Resource resource;
+
+	@BeforeEach
+	public void setup(AemContext context) throws Exception
+	{
+
+		page = context.create().page(CONTENT_TEST_RESOURCE);
+		resource = context.create().resource(page, "serviceItem",
+				RESOURCE_TYPE, COMPONENT_RESOURCE_TYPE,
+				HEADLINE, EXPECTED_HEADLINE,
+				LINK_URL_2, EXPECTED_LINK_URL_2);
+
+		homeStageItem = resource.adaptTo(HomeStageItem.class);
+	}
+
+	@Test
+	void testHomeStageItemHeadline() throws Exception
+	{
+		assertNotNull(homeStageItem);
+		assertEquals(EXPECTED_HEADLINE, homeStageItem.getHeadline());
+	}
+
+	@Test
+	void testHomeStageItemLinkURL2() throws Exception
+	{
+		assertNotNull(homeStageItem);
+		assertEquals(EXPECTED_LINK_URL_2, homeStageItem.getLinkURL2());
+	}
+
+}
---
+++ core/src/test/java/com/techem/core/models/HomeStageModelTest.java
@@ -0,0 +1,50 @@
+package com.techem.core.models;
+
+import com.day.cq.wcm.api.Page;
+import io.wcm.testing.mock.aem.junit5.AemContext;
+import io.wcm.testing.mock.aem.junit5.AemContextExtension;
+import org.apache.sling.api.resource.Resource;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(AemContextExtension.class)
+class HomeStageModelTest
+{
+
+	private HomeStageModel homeStageModel;
+
+	private static final String CONTENT_TEST_RESOURCE = "/content/mypage";
+	private static final String RESOURCE_TYPE = "sling:resourceType";
+	private static final String COMPONENT_RESOURCE_TYPE = "techem/components/service-item";
+
+	private static final String TRANSITION_SPEED = "headline";
+
+	private static final String EXPECTED_TRANSITION_SPEED = "10000";
+
+	private Page page;
+	private Resource resource;
+
+	@BeforeEach
+	public void setup(AemContext context) throws Exception
+	{
+
+		page = context.create().page(CONTENT_TEST_RESOURCE);
+		resource = context.create().resource(page, "serviceItem",
+				RESOURCE_TYPE, COMPONENT_RESOURCE_TYPE,
+				TRANSITION_SPEED, EXPECTED_TRANSITION_SPEED);
+
+		homeStageModel = resource.adaptTo(HomeStageModel.class);
+	}
+
+	@Test
+	void testHomeStageModelTransitionSpeed() throws Exception
+	{
+		assertNotNull(homeStageModel);
+		assertEquals(EXPECTED_TRANSITION_SPEED, homeStageModel.getTransitionSpeed());
+	}
+
+}
