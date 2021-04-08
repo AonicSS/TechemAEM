04ad977b9e992e9e3bd0cf28f4e6c11ce6512053
AndreeaStoiu <andreea.gina.stoiu@ibm.com>
2021-04-08 11:08:57 +0300

    added default value for stage headline

--- core/src/main/java/com/techem/core/models/SectionPage.java
+++ core/src/main/java/com/techem/core/models/SectionPage.java
@@ -1,6 +1,7 @@
 package com.techem.core.models;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Default;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
@@ -8,6 +9,7 @@ import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 @Model(adaptables = {Resource.class}, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class SectionPage {
 
+    @Default(values = "[Configure the Stage Headline from page properties]")
     @ValueMapValue(name = "stageHeadline")
     private String stageHeadline;
 
