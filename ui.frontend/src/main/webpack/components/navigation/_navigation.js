8eb5832edb8d1fc797a00bdaae2bab7e2dd2e0ba
Balan, Alexandru <Alexandru.Marian.Balan@ibm.com>
2021-12-14 12:30:27 +0200

    Navigation update

--- ui.frontend/src/main/webpack/components/navigation/_navigation.js
+++ ui.frontend/src/main/webpack/components/navigation/_navigation.js
@@ -10,6 +10,7 @@
             headerDropdown: '.header__dropdown-content',
             navigationLink: '.navigation__link',
             closeButton: '.close',
+            langNav: '.cmp-languagenavigation',
             langNavBtn: '.cmp-languagenavigation__trigger',
             langNavActive: '.cmp-languagenavigation__item--active',
             portalNavBtn: '.header__portal-trigger',
@@ -18,7 +19,9 @@
             langNavContent: '.cmp-languagenavigation__group',
             headerButton: ".header__button",
             active: "navigation-active",
-            hidden: 'header__dropdown-hidden'
+            hidden: 'header__dropdown-hidden',
+            navHidden: 'header-hide-nav',
+            burgerClickRegion: '.burger-click-region'
         };
 
         this.atributes = {
@@ -29,17 +32,24 @@
         };
 
         this.init = function () {
+            this.burgermenuinit();
             this.showAndHideDropdown();
             this.closeDropDown();
             this.redirectButton();
             this.showLangNav();
             this.showPortalNav();
+            this.hideNavigationOnSizeChange();
         }.bind(this);
 
         this.showLangNav = function () {
             const $langNavBtn = this.$el.find(this.selectors.langNavBtn);
             const $langNavCmp = this.$el.find(this.selectors.langNavCmp);
             const $activeLang = $langNavCmp.find(this.selectors.langNavActive);
+
+            /* Don't show langnav if navigation div is empty -> there are <= 1 language navigation items present */
+            if(!$.trim($langNavCmp.html())) {
+                return;
+            }
 
             // set current lang. trigger text
             // LANG placeholder in author-mode, as active lang is not displayed in author by AEM
@@ -49,7 +59,7 @@
                 $langNavBtn.text($activeLang.text());
             }
 
-
+            $langNavCmp.parent().removeClass('hidden'); // Show Langnav
             $langNavBtn.removeClass('visibility-hidden');
 
             $langNavBtn.on('click', () => {
@@ -153,13 +163,83 @@
 
         }.bind(this);
 
+        this.injectLangNav = function ($destinationLangDiv) {
+            const $langNav = this.$el.find(this.selectors.langNav);
+            const $destinationDiv = this.$el.find($destinationLangDiv);
+            /* Inject langnav ONLY if the langnav element has content (lang nav items are > 1) */
+            if($.trim($langNav.html())) {
+                $destinationDiv.append($langNav);
+            }
+        }.bind(this);
+
+        this.burgermenuinit = function () {
+            const isNavHidden = this.$el.hasClass(this.selectors.navHidden);
+            const portalCmp = this.$el.find(this.selectors.portalNavCmp);
+            var langNav = this.$el.find(this.selectors.langNav);
+            var $burger = this.$el.find(this.selectors.burgerClickRegion);
+
+            if(isNavHidden && !$.trim(portalCmp.html()) && !$.trim(langNav.html())) {
+                $burger.hide();
+                return;
+            }
+
+            var clickDelay = 100,
+                clickDelayTimer = null;
+            $($burger).on('click', () => {
+                if (clickDelayTimer === null) {
+                    $('body').addClass('scroll-hidden');
+                    $burger.toggleClass('active');
+                    $burger.parent().toggleClass('is-open');
+                    $('nav.navigation').slideDown();
+    
+                    if (!$burger.hasClass('active')) {
+                        $burger.addClass('closing');
+                        $('body').removeClass('scroll-hidden');
+                        $('nav.navigation').slideUp();
+                    }else {
+                        this.injectLangNav('.header__lang-nav-mobile');
+                    }
+
+                    clickDelayTimer = setTimeout(() => {
+                        $burger.removeClass('closing');
+                        clearTimeout(clickDelayTimer);
+                        clickDelayTimer = null;
+                    }, clickDelay);
+                }
+            });
+        }.bind(this);
+
+        this.hideNavigationOnSizeChange = function () {
+            const isNavHidden = this.$el.hasClass(this.selectors.navHidden);
+            const burgerClickRegion = this.$el.find(this.selectors.burgerClickRegion);
+            var langNavContainerDesktop = this.$el.find('.header__lang-nav');
+            var langNavContainerMobile = this.$el.find('.header__lang-nav-mobile');
+            
+            /* Resize event listener to properly inject lang nav and hide burger menu when resizing window from phone view to desktop */
+            window.addEventListener('resize', () => {
+                var isBurgerActive = burgerClickRegion.hasClass('active');
+
+                if(window.innerWidth > 1024) {
+                    if(isNavHidden && isBurgerActive) {
+                        burgerClickRegion.click();
+                    }
+                    if(!$.trim(langNavContainerDesktop.html())) {
+                        this.injectLangNav('.header__lang-nav');
+                    }
+                }
+                if(window.innerWidth < 1024 && isBurgerActive && !$.trim(langNavContainerMobile.html())) {
+                    this.injectLangNav('.header__lang-nav-mobile');
+                }
+            }, true);
+        }.bind(this);
+
         this.init();
 
         return this;
     }
 
     /* ---------------------------------------------
-    search
+                        search
     --------------------------------------------- */
     let isVisible = false;
     function initSearchbar($searchBarWrapper) {
@@ -168,7 +248,7 @@
             const $iconClosed = $("#trigger-icon-closed");
             const $iconOpened = $("#trigger-icon-opened");
             const $overlay = $(".search-trigger__overlay");
-
+            
             isVisible = !isVisible;
 
             if (isVisible) {
@@ -207,7 +287,7 @@
             }, 500);
         });
     }
-
+    
     /* ---------------------------------------------
     scroll shrink
     --------------------------------------------- */
@@ -235,50 +315,15 @@
         }
         lastScroll = currentScroll;
     });
-    /* ---------------------------------------------
-    burger menu
-    --------------------------------------------- */
-    function injectLangNav($headerNav) {
-        const $langNav = $headerNav.find('.cmp-languagenavigation');
-        const $langNavMobilePlaceholder = $headerNav.find('.header__lang-nav-mobile');
-        $langNavMobilePlaceholder.append($langNav);
-    }
 
-    function burgermenuinit($headerNav) {
-        var clickDelay = 100,
-            clickDelayTimer = null;
-        $('.burger-click-region').on('click', function () {
-            if (clickDelayTimer === null) {
-                var $burger = $(this);
-                $('body').addClass('scroll-hidden');
-                $burger.toggleClass('active');
-                $burger.parent().toggleClass('is-open');
-                $('nav.navigation').slideDown();
-
-                if (!$burger.hasClass('active')) {
-                    $burger.addClass('closing');
-                    $('body').removeClass('scroll-hidden');
-                    $('nav.navigation').slideUp();
-                }
-                clickDelayTimer = setTimeout(function () {
-                    $burger.removeClass('closing');
-                    injectLangNav($headerNav);
-                    clearTimeout(clickDelayTimer);
-                    clickDelayTimer = null;
-                }, clickDelay);
-            }
-        });
-    }
     $(document).ready(function () {
         const $searchBarWrapper = $(".headerSearchBar");
         if($searchBarWrapper.length) {
             initSearchbar($searchBarWrapper);
         }
 
-        const $headerNav = $("header[data-component-name='navigation']").not('.header-hide-nav');
+        const $headerNav = $("header[data-component-name='navigation']");
         if ($headerNav.length) {
-            burgermenuinit($headerNav);
-
             if ($headerNav[0].dataset.initialized !== "true") {
                 new NotificationModule($headerNav[0]);
             }
