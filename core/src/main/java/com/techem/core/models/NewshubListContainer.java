52815710af0eb1355fe26d16c054881231a64ece
AndreeaStoiu <andreea.gina.stoiu@ibm.com>
2022-01-20 12:32:14 +0200

    salesp-628 newshub list component and related article update for tags - merge for release 2.21.0

--- core/src/main/java/com/techem/core/models/NewshubListContainer.java
+++ core/src/main/java/com/techem/core/models/NewshubListContainer.java
@@ -1,20 +1,8 @@
 package com.techem.core.models;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.annotation.PostConstruct;
-import javax.inject.Inject;
-
-import com.day.cq.commons.jcr.JcrConstants;
 import com.day.cq.wcm.api.NameConstants;
+import com.day.crx.JcrConstants;
 import com.google.common.collect.Lists;
-
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
@@ -26,8 +14,22 @@ import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Model(adaptables = Resource.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL, resourceType = "techem/components/newshub-related-article")
-public class NewshubRelatedArticle {
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Model(adaptables = Resource.class, adapters = NewshubListContainer.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL,
+        resourceType = "techem/components/newshub-list")
+public class NewshubListContainer {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
@@ -36,6 +38,15 @@ public class NewshubRelatedArticle {
 
     @SlingObject
     private ResourceResolver resourceResolver;
+
+    @ValueMapValue(name="containerHeadline")
+    private String containerHeadline;
+
+    @ValueMapValue(name="buttonLabel")
+    private String buttonLabel;
+
+    @ValueMapValue(name="buttonLink")
+    private String buttonLink;
 
     @ValueMapValue(name="insertionType")
     private String insertionType;
@@ -67,9 +78,38 @@ public class NewshubRelatedArticle {
                     return item.getValueMap().get(JcrConstants.JCR_PRIMARYTYPE).equals(NameConstants.NT_PAGE)
                             && item.getChild(JcrConstants.JCR_CONTENT).adaptTo(ValueMap.class).get("sling:resourceType")
                             .equals("techem/components/structure/news-page");
-                }).limit(maxArticles).collect(Collectors.toList());
+                }).collect(Collectors.toList());
                 filterByTags();
                 orderList();
+            }
+
+            if(insertionType.equals("manually")) {
+                if(articleListManually != null && articleListManually.size() > 0) {
+                    for(Stage arti : articleListManually) {
+                        Resource originArti = resourceResolver.getResource(arti.getPath().replace(".html", ""));
+
+                        if(originArti == null) { continue; }
+
+                        ValueMap artiVals = originArti.getChild(JcrConstants.JCR_CONTENT).getValueMap();
+
+                        if(arti.getHeadline() == null) {
+                            arti.setHeadline((String) artiVals.get("headline"));
+                        }
+
+                        if(arti.getCategory() == null) {
+                            arti.setCategory((String) artiVals.get("category"));
+                        }
+
+                        if(arti.getDateObject() == null) {
+                            GregorianCalendar calendar = (GregorianCalendar) artiVals.get("date");
+                            arti.setDateObj(calendar.getTime());
+                        }
+
+                        if(arti.getText() == null) {
+                            arti.setText((String) artiVals.get("text"));
+                        }
+                    }
+                }
             }
         }
     }
@@ -109,10 +149,10 @@ public class NewshubRelatedArticle {
                     newsArticles.sort(Comparator.comparing(o -> ((Stage) o).getCreatedDate()));
                     break;
                 case "artiTitle":
-                    newsArticles.sort(Comparator.comparing(o -> ((Stage) o).getHeadline().toLowerCase().replaceAll("\\s+","")));
+                    newsArticles.sort(Comparator.comparing(o -> ((Stage) o).getHeadline().toLowerCase().replaceAll("\\s+", "")));
                     break;
                 case "artiCategory":
-                    newsArticles.sort(Comparator.comparing(o -> ((Stage) o).getCategory().toLowerCase().replaceAll("\\s+","")));
+                    newsArticles.sort(Comparator.comparing(o -> ((Stage) o).getCategory().toLowerCase().replaceAll("\\s+", "")));
                     break;
                 default: break;
             }
@@ -120,6 +160,7 @@ public class NewshubRelatedArticle {
             if(sortOrder.equals("descending")) {
                 Collections.reverse(newsArticles);
             }
+            newsArticles = newsArticles.stream().limit(maxArticles).collect(Collectors.toList());
         }
     }
 
@@ -145,6 +186,18 @@ public class NewshubRelatedArticle {
 
     public List<Stage> getArticleListManually() {
         return articleListManually;
+    }
+
+    public String getContainerHeadline() {
+        return containerHeadline;
+    }
+
+    public String getButtonLabel() {
+        return buttonLabel;
+    }
+
+    public String getButtonLink() {
+        return buttonLink;
     }
 
     public String getInsertionType() {
--- core/src/main/java/com/techem/core/models/Stage.java
+++ core/src/main/java/com/techem/core/models/Stage.java
@@ -1,11 +1,16 @@
 package com.techem.core.models;
 
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 import javax.annotation.PostConstruct;
+
+import com.day.cq.commons.jcr.JcrConstants;
+
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -14,9 +19,12 @@ import java.util.Objects;
 import static com.day.cq.wcm.api.NameConstants.PN_PAGE_LAST_MOD;
 
 @Model(adaptables = Resource.class,  defaultInjectionStrategy =  DefaultInjectionStrategy.OPTIONAL)
-public class Stage{
+public class Stage {
 
     private static final String DATE_PATTERN = "dd.MM.yyy";
+
+    @SlingObject
+    private ResourceResolver resourceResolver;
 
     @ValueMapValue(name = "headline")
     private String headline;
@@ -29,6 +37,12 @@ public class Stage{
 
     @ValueMapValue(name = PN_PAGE_LAST_MOD)
     private Date lastModifiedObject;
+
+    @ValueMapValue(name = JcrConstants.JCR_CREATED)
+    private Date createdDate;
+
+    @ValueMapValue(name = "path")
+    private String path;
 
     @ValueMapValue(name = "text")
     private String text;
@@ -50,6 +64,8 @@ public class Stage{
 
     public Date getLastModifiedObject() { return  Objects.nonNull(lastModifiedObject) ? (Date) lastModifiedObject.clone() : null;}
 
+    public Date getCreatedDate() { return  Objects.nonNull(createdDate) ? (Date) createdDate : null; }
+
     public String getHeadline() {
         return headline;
     }
@@ -60,6 +76,40 @@ public class Stage{
 
     public String getCategory() {
         return category;
+    }
+
+    public String getPath() {
+        if (path != null) {
+            Resource pathResource = resourceResolver.getResource(path);
+            // check if resource exists and link is internal
+            if (pathResource != null) {
+                path = path + ".html";
+            }
+        }
+        return path;
+    }
+
+    public void setPath(String p) {
+        path = p;
+    }
+
+    public void setText(String t) {
+        text = t;
+    }
+
+    public void setHeadline(String headLine) {
+        if(headLine != null && !(headLine.trim().length() == 0)) { headline = headLine; }
+    }
+
+    public void setCategory(String categoryStr) {
+        if(categoryStr != null && !(categoryStr.trim().length() == 0)) { category = categoryStr; }
+    }
+
+    public void setDateObj(Object dateObj) {
+        if(dateObj != null) {
+            DateFormat formatter = new SimpleDateFormat(DATE_PATTERN);
+            date = formatter.format(dateObj);
+        }
     }
 
     public String getDate() { return date; }
